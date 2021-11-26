2ec8dd6206f23ead0c4fd23cb1918c0ce3c2f74d
dMosca <devinmosca@yahoo.com>
2021-11-25 18:48:16 -0700

    Updated vehicle model

--- server/models/Vehicle.js
+++ server/models/Vehicle.js
@@ -1,36 +1,40 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const { Schema } = mongoose;
 
 const vehicleSchema = new Schema({
-    type: {
-        type: String,
-        required: true,
-        trim: true
-    },
-    year: {
-        type: Number,
-        required: true,
-    },
-    make: {
-        type: String,
-        required: true,
-        trim: true
-    },
-    model: {
-        type: String,
-        required: true,
-        trim: true
-    },
-    trimLvl: {
-        type: String,
-        trim: true
-    },
-    engineDisp: {
-        type: Number,
-    }
+  type: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  year: {
+    type: Number,
+    required: true,
+  },
+  make: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  model: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  trimLvl: {
+    type: String,
+    trim: true,
+  },
+  engineDisp: {
+    type: Number,
+  },
+  transaction: {
+    type: Schema.Types.ObjectId,
+    ref: "Transaction",
+  },
 });
 
-const Vehicle = mongoose.model('Vehicle', vehicleSchema);
+const Vehicle = mongoose.model("Vehicle", vehicleSchema);
 
 module.exports = Vehicle;
