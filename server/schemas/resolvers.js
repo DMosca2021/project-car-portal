43fef5413c713bb5f8e5ced71a58ae8dca9e8617
dMosca <devinmosca@yahoo.com>
2021-11-27 19:29:15 -0700

    Imported Todo for resolvers.js

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,5 +1,5 @@
 const { AuthenticationError } = require('apollo-server-express');
-const { User, Project, Vehicle, Transaction, Note } = require('../models');
+const { User, Project, Vehicle, Transaction, Note, Todo } = require('../models');
 const { signToken } = require('../utils/auth');
 // const stripe = require('stripe')('sk_test_4eC39HqLyjWDarjtT1zdp7dc');
 
