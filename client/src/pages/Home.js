66c78651cd9bdc66cccccdf97cdd4e5ecd164e94
dMosca <devinmosca@yahoo.com>
2021-11-29 20:56:04 -0700

    Updated Home.js

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -3,6 +3,9 @@ import React from "react";
 const Home = () => {
   return (
     <div className="container">
+      <br></br>
+      <h1>Welcome to the Project car Portal!<br></br>
+      Currently I am getting this project off the jackstands so check back soon for an updated page</h1>
     </div>
   );
 };
