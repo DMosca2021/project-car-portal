17ec1ec0a7ee9c3dcc2adb575fca04e026d53338
dMosca <devinmosca@yahoo.com>
2021-12-03 14:07:26 -0700

    Removed comments from Nav component

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -14,7 +14,6 @@ function Nav() {
             <Link to="/">← Back to Home</Link>
           </li>
           <li className="mx-1">
-            {/* this is not using the Link component to logout or user and then refresh the application to the start */}
             <a href="/" onClick={() => Auth.logout()}>
               Logout
             </a>
