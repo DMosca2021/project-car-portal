a5c3e0e591abcdcc776dacf3ec1967c8da09e676
dMosca <devinmosca@yahoo.com>
2021-12-05 12:19:08 -0700

    Removed bulma component from CurrentProjects, Working on project form functionality

--- client/src/pages/ProjectForm.js
+++ client/src/pages/ProjectForm.js
@@ -4,7 +4,8 @@ import { useMutation } from "@apollo/client";
 import { ADD_PROJECT, ADD_VEHICLE } from "../utils/mutations";
 
 function AddProject(props) {
-  const [formState, setFormState] = useState({ //props?
+  const [formState, setFormState] = useState({
+    //props?
     name: "",
     description: "",
     budget: "",
@@ -14,6 +15,7 @@ function AddProject(props) {
 
   const handleFormSubmit = async (event) => {
     event.preventDefault();
+    console.log(event.target.value);
     const mutationResponse = await addProject({
       variables: {
         name: formState.name,
@@ -24,9 +26,13 @@ function AddProject(props) {
     });
     const token = mutationResponse.data.addUser.token;
     Auth.login(token);
+    console.log(mutationResponse);
+    console.log(token);
+    console.log(Auth.login(token));
   };
 
   const handleChange = (event) => {
+    console.log(event.target.value);
     const { name, value } = event.target;
     setFormState({
       ...formState,
@@ -45,6 +51,210 @@ function AddProject(props) {
 
   return (
     <>
+      <form class="ui form">
+        <h4 class="ui dividing header">Shipping Information</h4>
+        <div class="field">
+          <label>Name</label>
+          <div class="two fields">
+            <div class="field">
+              <input
+                type="text"
+                name="shipping[first-name]"
+                placeholder="First Name"
+              ></input>
+            </div>
+            <div class="field">
+              <input
+                type="text"
+                name="shipping[last-name]"
+                placeholder="Last Name"
+              ></input>
+            </div>
+          </div>
+        </div>
+        <div class="field">
+          <label>Billing Address</label>
+          <div class="fields">
+            <div class="twelve wide field">
+              <input
+                type="text"
+                name="shipping[address]"
+                placeholder="Street Address"
+              ></input>
+            </div>
+            <div class="four wide field">
+              <input
+                type="text"
+                name="shipping[address-2]"
+                placeholder="Apt #"
+              ></input>
+            </div>
+          </div>
+        </div>
+        <div class="two fields">
+          <div class="field">
+            <label>State</label>
+            <select class="ui fluid dropdown">
+              <option value="">State</option>
+            </select>
+          </div>
+          <div class="field">
+            <label>Country</label>
+            <div class="ui fluid search selection dropdown">
+              <input type="hidden" name="country"></input>
+              <i class="dropdown icon"></i>
+              <div class="default text">Select Country</div>
+              <div class="menu"></div>
+            </div>
+          </div>
+        </div>
+        <h4 class="ui dividing header">Billing Information</h4>
+        <div class="field">
+          <label>Card Type</label>
+          <div class="ui selection dropdown">
+            <input type="hidden" name="card[type]"></input>
+            <div class="default text">Type</div>
+            <i class="dropdown icon"></i>
+            <div class="menu">
+              <div class="item" data-value="visa">
+                <i class="visa icon"></i>
+                Visa
+              </div>
+              <div class="item" data-value="amex">
+                <i class="amex icon"></i>
+                American Express
+              </div>
+              <div class="item" data-value="discover">
+                <i class="discover icon"></i>
+                Discover
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="fields">
+          <div class="seven wide field">
+            <label>Card Number</label>
+            <input
+              type="text"
+              name="card[number]"
+              maxlength="16"
+              placeholder="Card #"
+            ></input>
+          </div>
+          <div class="three wide field">
+            <label>CVC</label>
+            <input
+              type="text"
+              name="card[cvc]"
+              maxlength="3"
+              placeholder="CVC"
+            ></input>
+          </div>
+          <div class="six wide field">
+            <label>Expiration</label>
+            <div class="two fields">
+              <div class="field">
+                <select
+                  class="ui fluid search dropdown"
+                  name="card[expire-month]"
+                >
+                  <option value="">Month</option>
+                  <option value="1">January</option>
+                  <option value="2">February</option>
+                  <option value="3">March</option>
+                  <option value="4">April</option>
+                  <option value="5">May</option>
+                  <option value="6">June</option>
+                  <option value="7">July</option>
+                  <option value="8">August</option>
+                  <option value="9">September</option>
+                  <option value="10">October</option>
+                  <option value="11">November</option>
+                  <option value="12">December</option>
+                </select>
+              </div>
+              <div class="field">
+                <input
+                  type="text"
+                  name="card[expire-year]"
+                  maxlength="4"
+                  placeholder="Year"
+                ></input>
+              </div>
+            </div>
+          </div>
+        </div>
+        <h4 class="ui dividing header">Receipt</h4>
+        <div class="field">
+          <label>Send Receipt To:</label>
+          <div class="ui fluid multiple search selection dropdown">
+            <input type="hidden" name="receipt"></input>
+            <i class="dropdown icon"></i>
+            <div class="default text">Saved Contacts</div>
+            <div class="menu">
+              <div class="item" data-value="jenny" data-text="Jenny">
+                <img
+                  class="ui mini avatar image"
+                  src="/images/avatar/small/jenny.jpg"
+                ></img>
+                Jenny Hess
+              </div>
+              <div class="item" data-value="elliot" data-text="Elliot">
+                <img
+                  class="ui mini avatar image"
+                  src="/images/avatar/small/elliot.jpg"
+                ></img>
+                Elliot Fu
+              </div>
+              <div class="item" data-value="stevie" data-text="Stevie">
+                <img
+                  class="ui mini avatar image"
+                  src="/images/avatar/small/stevie.jpg"
+                ></img>
+                Stevie Feliciano
+              </div>
+              <div class="item" data-value="christian" data-text="Christian">
+                <img
+                  class="ui mini avatar image"
+                  src="/images/avatar/small/christian.jpg"
+                ></img>
+                Christian
+              </div>
+              <div class="item" data-value="matt" data-text="Matt">
+                <img
+                  class="ui mini avatar image"
+                  src="/images/avatar/small/matt.jpg"
+                ></img>
+                Matt
+              </div>
+              <div class="item" data-value="justen" data-text="Justen">
+                <img
+                  class="ui mini avatar image"
+                  src="/images/avatar/small/justen.jpg"
+                ></img>
+                Justen Kitsune
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="ui segment">
+          <div class="field">
+            <div class="ui toggle checkbox">
+              <input
+                type="checkbox"
+                name="gift"
+                tabindex="0"
+                class="hidden"
+              ></input>
+              <label>Do not include a receipt in the package</label>
+            </div>
+          </div>
+        </div>
+        <div class="ui button" tabindex="0">
+          Submit Order
+        </div>
+      </form>
+      {/* ---------------------------------------------------------------------------------------------- */}
       <form onSubmit={handleFormSubmit}>
         <div className="field">
           <label className="label" htmlFor="name">
@@ -52,7 +262,7 @@ function AddProject(props) {
           </label>
           <div className="control">
             <input
-              placeholder="Project Name"
+              placeholder=""
               name="name"
               type="name"
               id="projectName"
@@ -67,7 +277,7 @@ function AddProject(props) {
           </label>
           <div className="control">
             <input
-              placeholder="Project Description"
+              placeholder=""
               name="description"
               type="description"
               id="description"
@@ -96,7 +306,7 @@ function AddProject(props) {
           </label>
           <div className="control">
             <input
-              placeholder="What's your budget?"
+              placeholder=""
               name="budget"
               type="budget"
               id="budget"
@@ -111,7 +321,7 @@ function AddProject(props) {
           </label>
           <div className="control">
             <input
-              placeholder="How many hours have you spent working?"
+              placeholder=""
               name="timeSpent"
               type="timeSpent"
               id="time"
