595395fa167584bc447ef38928d0e3cf3efa3408
dMosca <devinmosca@yahoo.com>
2021-12-06 14:40:31 -0700

    Not found page styling complete

--- client/src/pages/NoMatch.js
+++ client/src/pages/NoMatch.js
@@ -1,5 +1,10 @@
 import React from "react";
 import Jumbotron from "../components/Jumbotron";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCarCrash } from '@fortawesome/free-solid-svg-icons';
+import { faCarSide } from '@fortawesome/free-solid-svg-icons';
+import { faSmog } from '@fortawesome/free-solid-svg-icons';
+import { faHighlighter } from '@fortawesome/free-solid-svg-icons';
 
 const NoMatch = () => {
   return (
@@ -8,9 +13,9 @@ const NoMatch = () => {
         <h1>
           <div className="hero is-halfheight" id="home-container">
           <h1>404 Page Not Found</h1>
-          <span role="img" aria-label="Face With Rolling Eyes Emoji">
-            🙄
-          </span>
+          <div>
+          _____<FontAwesomeIcon icon={faSmog} /><FontAwesomeIcon icon={faCarSide} /><FontAwesomeIcon icon={faCarCrash} />_<FontAwesomeIcon icon={faHighlighter} />
+          </div>
           </div>
         </h1>
       </Jumbotron>
