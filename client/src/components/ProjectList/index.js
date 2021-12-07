0c5c4c819bd78825d72ad7611499b0f72b90bcdc
dMosca <devinmosca@yahoo.com>
2021-12-06 22:17:22 -0700

    Formatted code in projectlist

--- client/src/components/ProjectList/index.js
+++ client/src/components/ProjectList/index.js
@@ -1,33 +1,29 @@
-import React, { useEffect } from 'react';
-import { useQuery } from '@apollo/client';
-import { QUERY_PROJECTS } from '../../utils/queries';
-import { idbPromise } from '../../utils/helpers';
+import React, { useEffect } from "react";
+import { useQuery } from "@apollo/client";
+import { QUERY_PROJECTS } from "../../utils/queries";
+import { idbPromise } from "../../utils/helpers";
 
-  const ProjectList = ({ projects }) => {
-    console.log(projects)
-    if (!projects.length) {
-      return <h3>No Projects Yet</h3>;
-    }
+const ProjectList = ({ projects }) => {
+  console.log(projects);
+  if (!projects.length) {
+    return <h3>No Projects Yet</h3>;
+  }
 
   return (
     <div className="card">
       <h2>Your projects:</h2>
-      {projects && 
+      {projects &&
         projects.map((project) => (
           <div key={project._id}>
             <h3>Lets See if this works?!?</h3>
-            <ul> 
-              <li>
-              {project.name}
-              </li>
-              <li>
-                {project.description}
-              </li>
+            <ul>
+              <li>{project.name}</li>
+              <li>{project.description}</li>
             </ul>
           </div>
         ))}
     </div>
   );
-}
+};
 
 export default ProjectList;
