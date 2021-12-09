37da8db65d10768f710d6183e2a64088cb22d90c
dMosca <devinmosca@yahoo.com>
2021-12-08 22:46:45 -0700

    Added CSS to signup page

--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -1,11 +1,26 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
-import { useMutation } from '@apollo/client';
-import Auth from '../utils/auth';
-import { ADD_USER } from '../utils/mutations';
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { useMutation } from "@apollo/client";
+import Auth from "../utils/auth";
+import { ADD_USER } from "../utils/mutations";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faStopwatch,
+  faUpload,
+  faImages,
+  faDollarSign,
+  faCalendarAlt,
+  faHome,
+  faHighlighter,
+} from "@fortawesome/free-solid-svg-icons";
 
 function Signup(props) {
-  const [formState, setFormState] = useState({ firstName: '', lastName: '', email: '', password: '' });
+  const [formState, setFormState] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    password: "",
+  });
   const [addUser] = useMutation(ADD_USER);
 
   const handleFormSubmit = async (event) => {
@@ -16,7 +31,6 @@ function Signup(props) {
         lastName: formState.lastName,
         email: formState.email,
         password: formState.password,
-
       },
     });
     const token = mutationResponse.data.addUser.token;
@@ -32,59 +46,81 @@ function Signup(props) {
   };
 
   return (
-    <div className="hero is-halfheight" id="form-container">
-      <Link to="/login">← Go to Login</Link>
+    <div className="columns is-mobile">
+      <div className="column is-10 is-offset-1" id="form-container">
+        <form className="hero is-halfheight" onSubmit={handleFormSubmit}>
+          <h1 className="column is-4 is-offset-5">
+            Add a project here _<FontAwesomeIcon icon={faHighlighter} />
+          </h1>
+          <div className="column is-4 is-offset-4">
+            <div className="field">
+              <label htmlFor="firstName">First Name:</label>
+              <input
+                className="input is-small"
+                placeholder="First"
+                name="firstName"
+                type="firstName"
+                id="firstName"
+                onChange={handleChange}
+              />
+            </div>
+          </div>
+          <div className="column is-4 is-offset-4">
+            <div className="field">
+              <label htmlFor="lastName">Last Name:</label>
+              <input
+                className="input is-small"
+                placeholder="Last"
+                name="lastName"
+                type="lastName"
+                id="lastName"
+                onChange={handleChange}
+              />
+            </div>
+          </div>
 
-      <h2>Signup</h2>
-      <form onSubmit={handleFormSubmit}>
-        <div className="field">
-          <label htmlFor="firstName">First Name:</label>
-          <input
-          className="input is-small"
-            placeholder="First"
-            name="firstName"
-            type="firstName"
-            id="firstName"
-            onChange={handleChange}
-          />
-        </div>
-        <div className="field">
-          <label htmlFor="lastName">Last Name:</label>
-          <input
-          className="input is-small"
-            placeholder="Last"
-            name="lastName"
-            type="lastName"
-            id="lastName"
-            onChange={handleChange}
-          />
-        </div>
-        <div className="field">
-          <label htmlFor="email">Email:</label>
-          <input
-          className="input is-small"
-            placeholder="youremail@test.com"
-            name="email"
-            type="email"
-            id="email"
-            onChange={handleChange}
-          />
-        </div>
-        <div className="field">
-          <label htmlFor="pwd">Password:</label>
-          <input
-          className="input is-small"
-            placeholder="******"
-            name="password"
-            type="password"
-            id="pwd"
-            onChange={handleChange}
-          />
-        </div>
-        <div className="flex-row flex-end">
-          <button type="submit">Submit</button>
-        </div>
-      </form>
+          <div className="column is-4 is-offset-4">
+            <div className="field">
+              <label htmlFor="email">Email:</label>
+              <input
+                className="input is-small"
+                placeholder="youremail@test.com"
+                name="email"
+                type="email"
+                id="email"
+                onChange={handleChange}
+              />
+            </div>
+          </div>
+          <div className="column is-4 is-offset-4">
+            <div className="field">
+              <label htmlFor="pwd">Password:</label>
+              <input
+                className="input is-small"
+                placeholder="******"
+                name="password"
+                type="password"
+                id="pwd"
+                onChange={handleChange}
+              />
+            </div>
+          </div>
+          <div className="column is-full" id="submit-btn">
+            <div className="field is-grouped is-grouped-centered">
+              <div className="buttons">
+                <button className="button is-dark" type="submit">
+                  Submit
+                </button>
+                
+                <Link className="button is-dark" to="/">
+                  <FontAwesomeIcon icon={faHome} />
+                  Home
+                </Link>
+              </div>
+            </div>
+          </div>
+        </form>
+      </div>
     </div>
   );
 }
